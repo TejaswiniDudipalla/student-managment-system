00321d0fd38ca4ca9f2b97ff80bca7d94d97e49a
TejaswiniDudipalla <116793939+TejaswiniDudipalla@users.noreply.github.com>
2023-01-10 16:54:46 +0100

    Add program files

---
+++ Student Managment system/Student Managment system/Controllers/loginController.cs
@@ -0,0 +1,85 @@
+﻿using Student_Managment_system.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApplication44.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        student_dbEntities1 db = new student_dbEntities1();
+
+
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User objchk)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                using (student_dbEntities1 db = new student_dbEntities1())
+                {
+
+                    var obj = db.Users.Where(a => a.user_name.Equals(objchk.user_name) && a.püassword.Equals(objchk.püassword)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.user_name.ToString();
+                        return RedirectToAction("Index", "Home");
+
+
+                    }
+
+                    else
+                    {
+
+                        ModelState.AddModelError("", "The UserName or Password Incorrect");
+
+
+
+                    }
+
+
+                }
+
+
+            }
+
+            return View(objchk);
+        }
+
+
+        public ActionResult Logout()
+        {
+
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
